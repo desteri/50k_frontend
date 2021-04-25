ad62ae7e2d05dca90ba21d046c5cd722a6382628
desteri <kyubei.dark@gmail.com>
2021-04-26 02:07:20 +0600

    added 36 classwork

---
+++ JS/classwork_36/js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let str = "text";
+let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(strObj);
+
+let array = [1, 2, 3];
+
+// console.log(array);
+
+let car = {
+    kuzov: "Железо",
+    kolesa: "4 колеса",
+    modal: function() {
+        console.log("text");
+    }
+};
+
+// let mb = {
+//     marka: "Мерс",
+//     year: "2021"
+// };
+
+let mb = Object.create(car);
+
+mb = {
+    marka: "Мерс",
+    year: "2021"
+};
+
+// mb.__proto__ = car;
+
+// Object.setPrototypeOf(mb, car);
+
+
+
+console.log(mb.modal);
+
+mb.modal();
