be975c57ce386685bbd5a01c97c78fc81ee4704c
desteri <kyubei.dark@gmail.com>
2021-05-12 14:22:28 +0600

    added 42 classwork

---
+++ JS/classwork_42/js/script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// let str = "text";
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// // console.log(strObj);
+
+// let array = [1, 2, 3];
+
+// // console.log(array);
+
+// let car = {
+//     kuzov: "Железо",
+//     kolesa: "4 колеса",
+//     modal: function() {
+//         console.log("text");
+//     }
+// };
+
+// // let mb = {
+// //     marka: "Мерс",
+// //     year: "2021"
+// // };
+
+// let mb = Object.create(car);
+
+// // mb.__proto__ = car;
+
+// // Object.setPrototypeOf(mb, car);
+
+
+
+// console.log(mb.kuzov);
+
+// // mb.modal();
+// let number = +"1";
+// console.log(number + 5);
+// // console.log(parseInt(number) + 5);
+// // console.log(typeof('1'));
+
+// let resInput = +prompt("Введите номер страницы:", '');
+// // // console.log(typeof(resInput));
+// // // if (typeof(resInput))
+
+// // console.log(resInput + 10);
+
+// // console.log(resInput);
+
+// console.log(`https://miukid.com/cat/${resInput}`);
+
+let click = document.getElementById('btn1');
+let click2 = document.getElementById('btn2');
+
+// click.onclick = function () {
+//     alert('Hi');
+// };
+
+let delElem = (ev) => {
+    alert(`Вы кликнули на кнопку: ${ev.target.innerText}`);
+};
+
+click.addEventListener('click', delElem);
+click2.addEventListener('click', delElem);
