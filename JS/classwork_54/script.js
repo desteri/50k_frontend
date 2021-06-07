01359ff60a7e1e5e062c9b9a96d9d4391c220981
desteri <kyubei.dark@gmail.com>
2021-06-07 19:43:54 +0600

    added 54 classwork

---
+++ JS/classwork_54/script.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+
+(function () {
+
+    'use strict';
+
+    // fetch('http://localhost:3000/products')
+    // .then(data => data.json())
+    // .then(res => console.log(res));
+
+    axios.get("http://localhost:3000/products")
+    .then(data => console.log(data));
+
+
+}());
