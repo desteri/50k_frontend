2cba76a9572b7d960b17b3b3821e9a6090fdde61
desteri <kyubei.dark@gmail.com>
2021-07-14 16:53:14 +0600

    added 62 classwork

---
+++ JS/classwork_62/src/index.js
@@ -0,0 +1,5 @@
+import * as data from "./js/main.js";
+import welcome from "./js/main.js";
+
+// console.log(`Hi ${data.name} ${data.sname}`);
+welcome();
---
+++ JS/classwork_62/src/js/main.js
@@ -0,0 +1,9 @@
+export let name = "Sanno";
+
+let sname = "Kai";
+
+export {sname};
+
+export default function welcome() {
+    console.log("Hi Sanno!");
+}
