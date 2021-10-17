e681e57c34352d19dbad60b36ff5368a421f65e3
desteri <kyubei.dark@gmail.com>
2021-10-17 17:39:17 +0600

    added 68 classwork

---
+++ React/classwork_68/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _script = require("./script.js");
+
+// const AutoSlider = require('./script');
+var newSlide = new _script.AutoSlider(450, 300, 4, true);
+newSlide.play();
+newSlide.whereAmI();
