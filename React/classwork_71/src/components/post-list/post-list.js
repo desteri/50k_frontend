0a68ecf6745598e8108f82f6468677fdb4877198
desteri <kyubei.dark@gmail.com>
2021-10-26 14:07:41 +0600

    added 71 classwork

---
+++ React/classwork_71/src/components/post-list/post-list.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PostListItem from '../post-list-item';
+import './post-list.scss';
+// import style from './app.module.css';
+
+const PostList = ({posts}) => {
+
+    const elem = posts.map((item) => {
+
+        const {id, ...itemProps} = item;
+
+        return (
+            <li key={id}>
+                <PostListItem {...itemProps} />
+            </li>
+        );
+
+    });
+
+    return (
+        <ul className="app-list list-group mt-5">
+            {elem}
+        </ul>
+    );
+}
+
+export default PostList;
